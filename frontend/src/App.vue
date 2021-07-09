ac903fe44ecae6519c9ef409481f3582de46d869
inabernic <bernic.in@gmail.com>
2021-07-09 12:33:03 +0200

    test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <h1>Hello</h1>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" nom="Ina"/>
+
+<div id="app">
+		<nav>
+			<HomeLink />
+			<a href="/about">À propos</a>
+			<a href="/contact">Contact</a>
+		</nav>
+		<p>Bienvenue sur notre page d'<HomeLink /></p>
+	</div>
+
+
+
+  <div id="app">
+		<nav>
+			<NavLink url="/" text="Accueil" />
+			<NavLink url="/about" text="À propos" />
+			<NavLink url="/contact" text="Contact" />
+		</nav>
+	</div>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeLink from './components/HomeLink.vue'
+import NavLink from './components/NavLink.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    HomeLink, NavLink
   }
 }
 </script>
