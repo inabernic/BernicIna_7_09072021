09d679e32764236aa94278412997f22d4f0e2d96
inabernic <bernic.in@gmail.com>
2021-07-09 11:58:24 +0200

    installation du projet, frontend avec VUE

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Hello</h1>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" nom="Ina"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
